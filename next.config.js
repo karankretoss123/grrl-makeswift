2a9570470b5985e76873c014f94449883a1f3714
rchintan7 <rchintan7@gmail.com>
2024-12-18 13:39:28 +0530

    build issue fix

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const withMakeswift = require('@makeswift/runtime/next/plugin')()
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    storeHash: 'b6whnxse2r',
+    storeHash: 'b6whnxse21r',
     accessToken: 'sztv42m0psb9nblrzuuu23myq5dj9lw',
     apiUrl: 'http://localhost:5555/',
     // apiUrl: `https://api.bigcommerce.com/stores/b6whnxse2r/v3/catalog/products`,
