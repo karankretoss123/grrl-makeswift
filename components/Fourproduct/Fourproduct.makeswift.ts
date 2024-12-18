87a3e30fba550707399840d587aba651f38a05cb
rchintan7 <rchintan7@gmail.com>
2024-12-18 18:47:15 +0530

    shop all fix category

--- components/Fourproduct/Fourproduct.makeswift.ts
+++ components/Fourproduct/Fourproduct.makeswift.ts
@@ -1,34 +1,36 @@
 import { lazy } from 'react'
 
-import { Select, Style } from '@makeswift/runtime/controls'
+import { List, Select, Shape, Slot, Style, TextInput } from '@makeswift/runtime/controls'
 import axios from 'axios'
 
 import { runtime } from '@/lib/makeswift/runtime'
 
-// Initial placeholder categories
-let categories: { label: string; value: any }[] = []
+let categories: { label: string; value: any }[] = [
+  {
+    label: 'Shop All',
+    value: '23',
+  },
+]
 
-// Fetch categories and update the options dynamically
-const fetchCategories = async () => {
-  try {
-    const apiUrl: string = process.env.apiUrl || ''
-    const response = await axios.get(`${apiUrl}v3/catalog/categories`, {
-      headers: {
-        'X-Auth-Token': process.env.accessToken,
-      },
-    })
-    categories = response.data.data.map((category: any) => ({
-      label: category.name,
-      value: category.id.toString(),
-    }))
-  } catch (error) {
-    console.error('Error fetching categories:', error)
-  }
-}
+// const fetchCategories = async () => {
+//   try {
+//     const apiUrl: string = process.env.apiUrl || ''
+//     const response = await axios.get(`${apiUrl}v3/catalog/categories`, {
+//       headers: {
+//         'X-Auth-Token': process.env.accessToken,
+//       },
+//     })
+//     categories = response.data.data.map((category: any) => ({
+//       label: category.name,
+//       value: category.id.toString(),
+//     }))
+//   } catch (error) {
+//     console.error('Error fetching categories:', error)
+//   }
+// }
+// fetchCategories()
+// .then(() => {
 
-fetchCategories()
-
-// Register the component synchronously with a placeholder
 runtime.registerComponent(
   lazy(() => import('./Fourproduct')),
   {
@@ -38,10 +40,9 @@ runtime.registerComponent(
       className: Style(),
       categoryId: Select({
         label: 'Category',
-        get options() {
-          return categories // Provide the current categories dynamically
-        },
+        options: categories, // Provide preloaded categories here
       }),
     },
   }
 )
+// })
