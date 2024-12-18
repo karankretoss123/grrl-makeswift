5537521727f3b2383f400280147f0598be010ad3
rchintan7 <rchintan7@gmail.com>
2024-12-18 13:18:03 +0530

    Four products compoentn

---
+++ components/Fourproduct/Fourproduct.makeswift.ts
@@ -0,0 +1,46 @@
+import { lazy } from 'react'
+
+import { List, Select, Shape, Slot, Style, TextInput } from '@makeswift/runtime/controls'
+import axios from 'axios'
+
+import { runtime } from '@/lib/makeswift/runtime'
+
+let categories: { label: string; value: any }[] = [
+  {
+    label: 'Test',
+    value: 'Test',
+  },
+]
+
+const fetchCategories = async () => {
+  try {
+    const apiUrl: string = process.env.apiUrl || ''
+    const response = await axios.get(`${apiUrl}v3/catalog/categories`, {
+      headers: {
+        'X-Auth-Token': process.env.accessToken,
+      },
+    })
+    categories = response.data.data.map((category: any) => ({
+      label: category.name,
+      value: category.id.toString(),
+    }))
+  } catch (error) {
+    console.error('Error fetching categories:', error)
+  }
+}
+fetchCategories().then(() => {
+  runtime.registerComponent(
+    lazy(() => import('./Fourproduct')),
+    {
+      type: 'Fourproduct',
+      label: 'Custom / Fourproduct',
+      props: {
+        className: Style(),
+        categoryId: Select({
+          label: 'Category',
+          options: categories, // Provide preloaded categories here
+        }),
+      },
+    }
+  )
+})
--- next.config.js
+++ next.config.js
@@ -3,6 +3,12 @@ const withMakeswift = require('@makeswift/runtime/next/plugin')()
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    storeHash: 'b6whnxse2r',
+    accessToken: 'sztv42m0psb9nblrzuuu23myq5dj9lw',
+    apiUrl: 'http://localhost:5555/',
+    // apiUrl: `https://api.bigcommerce.com/stores/b6whnxse2r/v3/catalog/products`,
+  },
 }
 
 module.exports = withMakeswift(nextConfig)
