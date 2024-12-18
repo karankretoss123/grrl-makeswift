44e19532a051c87949dc86f016e160014468fcfb
rchintan7 <rchintan7@gmail.com>
2024-12-18 14:09:33 +0530

    fetch change

--- components/Fourproduct/Fourproduct.makeswift.ts
+++ components/Fourproduct/Fourproduct.makeswift.ts
@@ -23,19 +23,20 @@ const fetchCategories = async () => {
     console.error('Error fetching categories:', error)
   }
 }
-fetchCategories().then(() => {
-  runtime.registerComponent(
-    lazy(() => import('./Fourproduct')),
-    {
-      type: 'Fourproduct',
-      label: 'Custom / Fourproduct',
-      props: {
-        className: Style(),
-        categoryId: Select({
-          label: 'Category',
-          options: categories, // Provide preloaded categories here
-        }),
-      },
-    }
-  )
-})
+fetchCategories()
+// .then(() => {
+runtime.registerComponent(
+  lazy(() => import('./Fourproduct')),
+  {
+    type: 'Fourproduct',
+    label: 'Custom / Fourproduct',
+    props: {
+      className: Style(),
+      categoryId: Select({
+        label: 'Category',
+        options: categories, // Provide preloaded categories here
+      }),
+    },
+  }
+)
+// })
