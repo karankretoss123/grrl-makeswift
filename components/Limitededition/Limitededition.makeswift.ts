c68f0d9ccf3da5736b08b82665ac75d78feb11af
rchintan7 <rchintan7@gmail.com>
2024-12-19 16:20:25 +0530

    category changes

--- components/Limitededition/Limitededition.makeswift.ts
+++ components/Limitededition/Limitededition.makeswift.ts
@@ -7,26 +7,37 @@ import { runtime } from '@/lib/makeswift/runtime'
 
 let categories: { label: string; value: any }[] = [
   {
-    label: 'Beanies',
-    value: '28',
+    label: 'Utility',
+    value: '22',
   },
   {
-    label: 'Accessories',
-    value: '27',
-  },
-  {
-    label: 'Bold Print Leggings',
-    value: '29',
-  },
-  {
-    label: "Collectors' Edition Close Out",
-    value: '30',
-  },
-  {
-    label: 'Squat Proof Leggings',
-    value: '46',
+    label: 'Shop All',
+    value: '23',
   },
 ]
+
+// let categories: { label: string; value: any }[] = [
+//   {
+//     label: 'Beanies',
+//     value: '28',
+//   },
+//   {
+//     label: 'Accessories',
+//     value: '27',
+//   },
+//   {
+//     label: 'Bold Print Leggings',
+//     value: '29',
+//   },
+//   {
+//     label: "Collectors' Edition Close Out",
+//     value: '30',
+//   },
+//   {
+//     label: 'Squat Proof Leggings',
+//     value: '46',
+//   },
+// ]
 
 // const fetchCategories = async () => {
 //   try {
