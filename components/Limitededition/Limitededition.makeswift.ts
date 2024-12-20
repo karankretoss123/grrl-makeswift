ac7ddeaa8c3540b6c62830c43463c63d036bda16
rchintan7 <rchintan7@gmail.com>
2024-12-20 16:52:28 +0530

    category live changes

--- components/Limitededition/Limitededition.makeswift.ts
+++ components/Limitededition/Limitededition.makeswift.ts
@@ -5,39 +5,39 @@ import axios from 'axios'
 
 import { runtime } from '@/lib/makeswift/runtime'
 
-let categories: { label: string; value: any }[] = [
-  {
-    label: 'Utility',
-    value: '22',
-  },
-  {
-    label: 'Shop All',
-    value: '23',
-  },
-]
-
 // let categories: { label: string; value: any }[] = [
 //   {
-//     label: 'Beanies',
-//     value: '28',
+//     label: 'Utility',
+//     value: '22',
 //   },
 //   {
-//     label: 'Accessories',
-//     value: '27',
-//   },
-//   {
-//     label: 'Bold Print Leggings',
-//     value: '29',
-//   },
-//   {
-//     label: "Collectors' Edition Close Out",
-//     value: '30',
-//   },
-//   {
-//     label: 'Squat Proof Leggings',
-//     value: '46',
+//     label: 'Shop All',
+//     value: '23',
 //   },
 // ]
+
+let categories: { label: string; value: any }[] = [
+  {
+    label: 'Beanies',
+    value: '28',
+  },
+  {
+    label: 'Accessories',
+    value: '27',
+  },
+  {
+    label: 'Bold Print Leggings',
+    value: '29',
+  },
+  {
+    label: "Collectors' Edition Close Out",
+    value: '30',
+  },
+  {
+    label: 'Squat Proof Leggings',
+    value: '46',
+  },
+]
 
 // const fetchCategories = async () => {
 //   try {
